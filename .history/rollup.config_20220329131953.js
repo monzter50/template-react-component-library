5503e985e789036b77d99c17b184cf6bc1c930dd
jose antonio <joseantonio.gonzalez@globant.com>
2022-03-29 13:39:42 -0600

    add config for testing and css

--- .history/rollup.config_20220329131953.js
+++ .history/rollup.config_20220329131953.js
@@ -3,8 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
 
@@ -24,18 +22,15 @@ export default [
       },
     ],
     plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: "./tsconfig.json" }),
-        postcss(),
-        terser(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
   },
 ];
--- .history/rollup.config_20220329132009.js
+++ .history/rollup.config_20220329132009.js
@@ -3,8 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
 
@@ -24,12 +22,10 @@ export default [
       },
     ],
     plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: "./tsconfig.json" }),
-        postcss(),
-        terser(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
